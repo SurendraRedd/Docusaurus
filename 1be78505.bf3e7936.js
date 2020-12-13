d692eda314d2fcdef7620427daa0f8b039a8710e
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2020-12-13 22:20:09 +0530

    Add files via upload

---
+++ 1be78505.bf3e7936.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{131:function(n,o,w){"use strict";var a=w(126);w.d(o,"a",(function(){return a.a}))}}]);
---
+++ 71dbfd8d.34b73b60.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{83:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/Docusaurus/blog/welcome"},{"title":"Hello","permalink":"/Docusaurus/blog/hello-world"},{"title":"Hola","permalink":"/Docusaurus/blog/hola"}]}')}}]);
